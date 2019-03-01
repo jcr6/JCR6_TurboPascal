4be44b3d1c0f8e691bf3b696a9f79afbf6176ac8
Tricky1975 <facebook@tbbs.nl>
2019-03-01 11:15:37 +0100

    License block

--- testjcr6.pas
+++ testjcr6.pas
@@ -1,3 +1,22 @@
+{ --- START LICENSE BLOCK ---
+***********************************************************
+testjcr6.pas
+This particular file has been released in the public domain
+and is therefore free of any restriction. You are allowed
+to credit me as the original author, but this is not 
+required.
+This file was setup/modified in: 
+2019
+If the law of your country does not support the concept
+of a product being released in the public domain, while
+the original author is still alive, or if his death was
+not longer than 70 years ago, you can deem this file
+"(c) Jeroen Broks - licensed under the CC0 License",
+with basically comes down to the same lack of
+restriction the public domain offers. (YAY!)
+*********************************************************** 
+Version 19.03.01
+  --- END LICENSE BLOCK --- } 
 Program TestJCR6;
 
 Uses
